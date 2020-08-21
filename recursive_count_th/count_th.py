42d3683dda9b57fbaaf4dc605bc9ab4dc1443c5f
David Molinari <david.m.molinari@gmail.com>
2020-08-21 13:01:19 -0400

    Finished project

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -7,56 +7,19 @@ def count_th(word):
     # TBC
     th_string = "th"
 
-    n1 = len(word); 
-    n2 = len(th_string); 
+    n1 = len(word)
+    n2 = len(th_string)
       
     # Base Case 
     if (n1 == 0 or n1 < n2): 
-        return 0; 
+        return 0
   
     # Recursive Case 
     # Checking if the first 
     # substring matches 
     if (word[0 : n2] == th_string): 
-        return count_th(word[n2 - 1:]) + 1; 
+        return count_th(word[n2 - 1:]) + 1
   
     # Otherwise, return the count  
     # from the remaining index 
-    return count_th(word[n2 - 1:]); 
-
-
-
-
-
-
-
-
-
-
-
-
-    # if (len(word) == 0 or len(word) < len('th')):
-    #     return 0
-    
-    # if (word[0: len('th')] == 'th'):
-    #     return count_th(word[len(word) -1:]) + 1
-    
-    # return count_th(word[len('th') - 1:])
-
-
-
-
-
-
-
-
-    # if word == None:
-    #     return None
-    
-    # cur_ind = 0
-    # count = 0
-    # cur_ind = word.find('th')
-    # if cur_ind == -1:
-    #     return count
-    # count += 1
-    # count_th(word[cur_ind+1:])
+    return count_th(word[n2 - 1:])
